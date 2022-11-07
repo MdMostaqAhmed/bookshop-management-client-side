3a1100b200fce95ac6cc2e80abe210618d410c7c
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 15:57:10 +0600

    load data from server and show data on clientside

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Products.css'
 
 const Products = () => {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        const url = 'http://localhost:5000/books';
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setItems(data))
+    }, [])
     return (
-        <div>
-            <h1>Products page</h1>
+        <div className='container'>
+            <h1 className='w-50 mx-auto'>Available Products</h1>
+            <div className='items-container'>
+                {
+                    items.map((item, index) => <div key={index} className='item'>
+                        <div className='bookImage'>
+                            <img src={item.img} alt="" />
+                        </div>
+                        <div className='ms-2 mt-2'>
+                            <h3>Book Name: {item.name}</h3>
+                            <hr class="style1"></hr>
+                            <h5><strong> Price: {item.price}</strong></h5>
+                            <hr class="style1"></hr>
+                            <spin><strong>Supplier: {item.supplier}</strong></spin> <br />
+                            <hr class="style1"></hr>
+                            <spin><strong>Available: {item.available} </strong></spin> <br />
+                            <hr class="style1"></hr>
+                            <spin><strong>Sold: {item.sold}</strong></spin> <br />
+                        </div>
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
