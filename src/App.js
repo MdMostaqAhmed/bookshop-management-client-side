7531a519770f5bc74aef856de8da34e9aaebced7
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 08:38:59 +0600

    header, footer and pageNotFound components added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Header from './Components/Shared/Header/Header';
+import Footer from './Components/Shared/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
-
+      <Footer></Footer>
     </div>
   );
 }
