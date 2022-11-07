d1e4c4ecd24e67f9d175fac8dedea4189cf25cce
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 10:21:41 +0600

    add All Products, Blog and About us pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './Components/Home/Home/Home';
 import Header from './Components/Shared/Header/Header';
 import Footer from './Components/Shared/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Products from './Components/Products/Products';
+import Blogs from './Components/Blogs/Blogs';
+import AboutUs from './Components/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -12,6 +15,10 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/about' element={<AboutUs></AboutUs>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -10,6 +10,9 @@ const Header = () => {
                     <Navbar.Brand as={Link} to="/">Navbar</Navbar.Brand>
                     <Nav className="me-auto">
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/products">All Products</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About Us</Nav.Link>
 
                     </Nav>
 
