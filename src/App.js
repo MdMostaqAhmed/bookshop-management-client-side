06a0ba4108cbacccf8ba9fa5cab1f2978bbbe4b1
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-07 08:32:01 +0600

    added home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
-      <h1>Book shop</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
 
     </div>
   );
