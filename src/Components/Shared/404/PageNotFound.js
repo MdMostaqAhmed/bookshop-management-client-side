9e7d37c6652e9ef4627355221bbad5fa36349d59
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-11-18 11:49:46 +0600

    footer problem solved

--- src/Components/Shared/404/PageNotFound.js
+++ src/Components/Shared/404/PageNotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageNotFound = () => {
     return (
-        <div>
+        <div className='mt-5 d-flex justify-content-center '>
             <h1>404 || Page Not Found</h1>
         </div>
     );
